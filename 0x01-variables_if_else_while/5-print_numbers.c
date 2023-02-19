1034912c27f9f51522bd727cfc4bfbc418256bed
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-02-19 03:58:09 -0800

    Last digit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,13 +5,20 @@
  *
  * Return: Always 0 (Success)
  */
- int main(void)
- {
- 	int a = 0;
- 	while (a < 10)
- 	{
- 		printf("%d", a);
- 		a++;
- 	}
- 	return (0);
- }
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 10)
+	{
+		if (a == 10)
+		{
+			printf("\n");
+			continue;
+		}
+		printf("%d", a);
+		a++;
+	}
+	return (0);
+}
