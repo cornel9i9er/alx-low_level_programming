14d18e23ae5467689a52792fb25a9fa09c81f9ab
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-02-19 03:38:26 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits from zero to 10
+ *
+ * Return: Always 0 (Success)
+ */
+ int main(void)
+ {
+ 	int a = 0;
+ 	while (a < 10)
+ 	{
+ 		printf("%d", a);
+ 		a++;
+ 	}
+ 	return (0);
+ }
