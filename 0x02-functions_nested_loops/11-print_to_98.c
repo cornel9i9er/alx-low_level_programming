04324e4e71d05755de69b07c0690a1077866b2d0
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-02-22 06:45:37 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,11 +17,11 @@ void print_to_98(int n)
 		{
 			if (n != 98)
 			{
-				printf("%d, ", n);
+				printf("%d, ", i);
 			}
 			else
 			{
-				printf("%d", n);
+				printf("%d ", i);
 			}
 		}
 	}
@@ -33,11 +33,11 @@ void print_to_98(int n)
 		{
 			if (n != 98)
 			{
-				printf("%d, ", n);
+				printf("%d, ", i);
 			}
 			else
 			{
-				printf("%d", n);
+				printf("%d ", i);
 			}
 		}
 	}
