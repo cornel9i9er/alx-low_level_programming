3cda09196f2b88bb4c393f654150e0acc81b62e5
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-02-23 07:30:01 -0800

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *main - fizz buzz
+ *
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+		printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ");
+		}
+		if (i == 100)
+		{
+			printf("\n");
+		}
+	}
+	return (0);
+}
