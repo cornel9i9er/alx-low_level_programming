7e60512adbc550918a299077915f53c2859d1571
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-02-23 04:50:30 -0800

    The shortest distance between two points is a straight li

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers
+ *
+ * Return: always 0
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+	
+       for (i = 0; i < 10; i++)
+       {
+		for (j = 48; j < 58; j++)
+		{
+			_putchar(j);
+		}
+	} 
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_line - prints a length based line
+ *@n: magic num
+ *
+ *Return: always 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n == 0 || n < 0)
+	{
+		_putchar(10);
+		return (0);
+	}
+	for (i = n; i > 0; i--)
+	{
+		_putchar('_');
+	}
+	_putchar(10);
+	return (0);
+}
