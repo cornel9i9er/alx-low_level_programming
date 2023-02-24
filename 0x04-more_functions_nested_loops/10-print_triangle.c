f1f275f22bca54055f6b892b26fe2735f6e56d7c
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-02-24 04:23:19 -0800

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,7 +19,7 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < size; j++)
+			for (j = 0; j <= size; j++)
 			{
 				int here = size - i;
 
@@ -34,6 +34,5 @@ void print_triangle(int size)
 			}
 			_putchar(10);
 		}
-		_putchar(10);
 	}
 }
