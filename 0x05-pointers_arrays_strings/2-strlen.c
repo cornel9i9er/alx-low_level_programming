3edbfae3b501f4e3253e302a57e5c5782e96725d
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-02-27 12:42:52 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen - len string
+ *@s: sring array
+ *
+ *Return: always 0
+ */
+int _strlen(char *s)
+{
+	int ret = 0;
+
+	while (s && (s++) != ’\0’)
+	{
+		ret++;
+	}
+	return (ret);
+}
+
