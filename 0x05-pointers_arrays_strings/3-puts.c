7589356205a3747724c54f4b8e861b88af1cf7a4
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-02-27 13:02:26 -0800

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+/**
+ *_puts - magic function
+ *@str: pointer to string vr
+ *
+ *Return: void return
+ */
+void _puts(char *str)
+{
+	while (str && (*str++) != '\0')
+	{
+		_putchar(*str);
+	}
+}
