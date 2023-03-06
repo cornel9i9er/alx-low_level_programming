02d5c33c4bea5c38737b471617bf9e9c8b54df3d
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-03-05 23:42:25 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset -writes a constant byte to ranged mem.
+ * @s: ranged mem pointer.
+ * @b: constant byte to be written.
+ * @n: actual range.
+ * Return: poniter to the mem block.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
