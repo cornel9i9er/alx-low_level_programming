114a124e2ae3a863a3b72ff835526877588eb31f
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-03-06 01:01:47 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string and returns it's mem address.
+ * @s: the string.
+ * @c: the character to seek
+ *
+ * Return: pointer to char c in s or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	int pos = 0;
+
+	while (1)
+	{
+		if (s[i] != '\0' && s[i] == c)
+		{
+			return (s + pos);
+		}
+		else if (s[i] == '\0')
+		{
+			return (NULL);
+		}
+		pos++;
+		i++;
+	}
+}
