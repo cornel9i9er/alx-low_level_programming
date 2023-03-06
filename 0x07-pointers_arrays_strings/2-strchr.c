6114b3d3ce1e36a7b372dd0096c23c37446a977a
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-03-06 03:52:12 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,7 +15,14 @@ char *_strchr(char *s, char c)
 
 	while (1)
 	{
-		if (s[i] != '\0' && s[i] == c)
+		if (c == '\0')
+		{
+			if (s[i] == '\0')
+			{
+				return (s + pos);
+			}
+		}
+		else if (s[i] != '\0' && s[i] == c)
 		{
 			return (s + pos);
 		}
