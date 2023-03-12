e06d105a0a3db86011fcf25b4a6067f2f9e41292
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-03-12 01:02:54 -0800

    putchar

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _strlen(char *s);
-int checkint(char c);
-long int _atoi(char *n, int len);
+
 long int _pow(int n, int i);
 int _putchar(char c);
+
+long int _atoi(char *n, int len);
+void _itoa(int n);
 #endif
