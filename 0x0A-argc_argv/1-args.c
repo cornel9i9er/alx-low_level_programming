8d532d21773e96a00f0a7b2ae85a84159af39c6d
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-03-13 19:53:11 -0700

    Silence is argument carried out by other means

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
+void *_itoa(int n, char *s);
 /**
  * main - the father of the program
  * @argc: contains command line arugument count
@@ -9,7 +10,74 @@
  */
 int main(int argc, char *argv[])
 {
-	(void) argv[];
-	printf("%d\n", argc - 1);
+	char c[10];
+	int i = 0;
+
+	_itoa(argc - 1, c);
+	(void) argv;
+	while (1)
+	{
+		if (*(c + i) != '\0')
+		{
+			write(1, c + i, sizeof(char));
+			i++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	_putchar(10);
 	return (0);
+}
+
+/**
+ * _itoa - prints out int using putchar
+ * @n: integer to print
+ * @s: pointer to an empty char aray
+ * Return: void.
+ */
+void *_itoa(int n, char *s)
+{
+	int i = 0;
+	char z;
+	int it = 0;
+	int k;
+	int j;
+	int len = 0;
+
+	while (1)
+	{
+		if (n < 10)
+		{
+			*(s + i) = 48 + n;
+			*(s + i + 1) = '\0';
+			break;
+		}
+		else
+		{
+			*(s + i) = 48 + (n % 10);
+			n -= n % 10;
+			n /= 10;
+			i++;
+		}
+	}
+	while (1)
+	{
+		if (*(s + it) != '\0')
+		{
+			len++;
+			it++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	for (k = 0; k < len / 2; k++)
+	{
+		z = *(s + k);
+		*(s + k) = *(s + len - 1 - k);
+		*(s + len - 1 - k) = z;
+	}
 }
