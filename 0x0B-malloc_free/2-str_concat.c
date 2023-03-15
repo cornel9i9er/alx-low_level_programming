6f4fdd890cdb8e9b4d63f06c8d75a65c5abdd337
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-03-14 17:28:23 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,80 +1,42 @@
 #include "main.h"
-#include <stddef.h>
 #include <stdlib.h>
-void *set(char *s, char *s1, char *s2, int len1, int len2);
 /**
- * str_concat - concatenates 2 str on allocated mem block
- * @s1: string 1
- * @s2: string 2
- *
- * Return: pointer to llocated mem block.
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
 {
-	int len1 = 0;
-	int len2 = 0;
-	int i = 0;
-	int itr = 0;
-	char *s;
+	char *conct;
+	int i, ci;
 
 	if (s1 == NULL)
-		*s1 = "";
-	else if (s2 == NULL)
-		*s2 = "";
-	while (1)
-	{
-		if (itr == 0)
-		{
-			if (*(s1 + i) != '\0')
-			{
-				len1++;
-				i++;
-			}
-			else
-			{
-				itr++;
-				i = 0;
-			}
-		}
-		else if (itr == 1)
-		{
-			if (*(s2 + i) != '\0')
-			{
-				len2++;
-				i++;
-			}
-			else
-			{
-				break;
-			}
-		}
-	}
-	s = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
+
+	if (conct == NULL)
 		return (NULL);
-	set(s, s1, s2, len1, len2);
-	return (s);
-}
-
-/**
- * set - does th cat magic
- * @s: allocated str block
- * @s1: str 1
- * @s2: str 2
- * @len1: str1 len
- * @len2: str2 len
- *
- * Return: void
- */
-void *set(char *s, char *s1, char *s2, int len1, int len2)
-{
-	int i;
-
-	for (i = 0; i < len1 + len2; i++)
+	i = ci = 0;
+	while (s1[i] != '\0')
 	{
-		if (i < len1)
-			*(s + i) = *(s1 + i);
-		else
-			*(s + i) = *(s2 + (i - len1));
+		conct[i] = s1[i];
+		i++;
 	}
+
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++, ci++;
+	}
+	conct[i] = '\0';
+	return (conct);
 }
