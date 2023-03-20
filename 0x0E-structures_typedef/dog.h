418db0f49b1e840b3d0d5e370322a05254877e46
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-03-20 07:35:58 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - contains dogs info
+ *
+ * @name: name of dog.
+ * @age: age of the dog.
+ * @owner: name of the owner.
+ * Description: longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
