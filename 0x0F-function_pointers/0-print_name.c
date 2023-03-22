537bc8b4f6cbc2a7011c09f946d75c2eb94c44dd
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-03-22 06:19:35 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - calls a passed function
+ *
+ * @name: name to be printed
+ * @f: function to call.
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
