5d8b56cbd5c6d322c6263e8a24f06bd2780b62b6
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-06-09 22:50:22 +0300

    MAIN_H

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int *array(int c, int arr[]);
+int _log(int n);
+int checker(char *s);
+void buffer(char *s);
 int _putchar(char c);
-int _atoi(char *s);
+
 #endif
