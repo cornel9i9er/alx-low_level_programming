4399b2ea5a175f907a744d0740e425c1b7810e35
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-06-13 12:58:14 +0300

    alocate mem for 2d array ++

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,8 +16,22 @@ int **alloc_grid(int width, int height)
 	if (height <= 0 || width <= 0)
 		return (NULL);
 	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+	{
+		free(grid);
+		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				free(grid[i]);
+			}
+			free(grid);
+			return (NULL);
+		}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
