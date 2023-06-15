0171590ef8dfe84106ca6f1a8d4669172828245e
cornel9i9er <corneliusmwirigi110@gmail.com>
2023-06-15 12:46:27 -0700

    no dereferencing void pointers. no aritmetics either

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,10 +1,16 @@
 #include "main.h"
-
+/**
+ * _calloc - allocates a block of mem.
+ *
+ * @nmemb: number of members in block.
+ * @size: size of each member.
+ *
+ * Return: pointer to allocated block or null on failure.
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr = NULL;
-	int i;
-	
+
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
@@ -12,10 +18,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		free(ptr);
 		return (NULL);
-	}
-	for (i = 0; i < nmemb; i++)
-	{
-		*(ptr + i) = 0;
 	}
 	return (ptr);
 }
